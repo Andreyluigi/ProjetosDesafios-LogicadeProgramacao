58a198cd54bcc6454f12e29d9c9aaf632a0da58b
Andreyluigi <andreybarro12@gmail.com>
2026-08-24 16:24:32 -0300

    implementando o codigo do desafios do sistema de ingressos

--- ingresso/js/app.js
+++ ingresso/js/app.js
@@ -1,51 +1,25 @@
-    let cadeiraInf = 400;
-    let cadeiraSup = 200;
-    let pista = 100;
-
 function comprar(){
-    let tipoIngresso = document.getElementById('tipo-ingresso').value;
-    let quantidade = document.getElementById('qtd').value;
-    console.log(tipoIngresso, quantidade)
-    // if (cadeiraInf>=quantidade && cadeiraInf>0){
-    //     alert(`Voce comprou ${quantidade} ingresso(s)`)
-    //     cadeiraInf = cadeiraInf - quantidade;
-    //     console.log(cadeiraInf)
-    // }
-    // else if (cadeiraSup >= quantidade && cadeiraSup > 0) {
-    //     alert(`Voce comprou ${quantidade} ingresso(s)`)
-    //     cadeiraSup = cadeiraSup - quantidade;
-    //     console.log(cadeiraSup)
-    // }
-    // else if (pista >= quantidade && pista > 0) {
-    //     alert(`Voce comprou ${quantidade} ingresso(s)`)
-    //     pista = pista - quantidade;
-    //     console.log(pista)
-    // }
-    if (tipoIngresso == 'inferior'){
-     if (cadeiraInf>=quantidade && cadeiraInf>0){
-         alert(`Voce comprou ${quantidade} ingresso(s)`)
-        cadeiraInf = cadeiraInf - quantidade;
-         console.log(cadeiraInf)
+    let tipo = document.getElementById('tipo-ingresso');
+    let qtd = document.getElementById('qtd').value;
+    
+    if (tipo.value == 'pista') {
+        comprarPista(qtd);
         }
-    }else {
-         alert("Quantidade de ingressos indisponível!")
+    if (tipo.value == 'superior'){
+        comprarSuperior(qtd)
     }
-    if (tipoIngresso == 'superior'){
-        if (cadeiraSup >= quantidade && cadeiraSup > 0) {
-            alert(`Voce comprou ${quantidade} ingresso(s)`)
-            cadeiraSup = cadeiraSup - quantidade;
-            console.log(cadeiraSup)
-        }
-    } else {
-        alert("Quantidade de ingressos indisponível!")
+    if (tipo.value == 'inferior'){
+        comprarInferior(qtd)
     }
-    if (tipoIngresso == 'pista'){
-        if (pista >= quantidade && pista > 0){
-            alert(`Voce comprou ${quantidade} ingresso(s)`)
-            pista = pista - quantidade;
-            console.log(pista)
-        }
+}
+
+function comprarPista(qtd) {
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+    if (qtd > qtdPista) {
+        alert('Quantidade indisponível para tipo pista');
     } else {
-        alert("Quantidade de ingressos indisponível!")
+        qtdPista = qtdPista - qtd;
+        document.getElementById('qtd-pista').textContent = qtdPista;
+        alert('Compra realizada com sucesso!');
     }
 }
